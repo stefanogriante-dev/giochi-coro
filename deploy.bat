1b7bf150941ef6c88cd611b08e84070c5d82c27f
Stefano <stefanogriante@gmail.com>
2026-07-05 13:07:40 +0200

    trigger deploy: Batti il Tempo 3 voci + 7 misure

--- deploy.bat
+++ deploy.bat
@@ -2,7 +2,7 @@
 cd /d "E:\Stefano\progetti_personali\giochi-coro"
 del /f /q .git\index.lock 2>nul
 git add .
-git commit -m "IngressiGame v4: layout Simon, stili inline, rimozione overlay rotazione"
+git commit --allow-empty -m "trigger deploy: Batti il Tempo 3 voci + 7 misure"
 git push
 echo.
 echo Deploy completato!
